5f51d6334d98ac84b8c0ad96d193322572a1bbee
159th-Ajax <ajfaske@consolidated.net>
2015-10-10 12:10:37 -0500

    config
    default mission folder

--- Scripts/Addons/ServMan3/serverconfig.lua
+++ Scripts/Addons/ServMan3/serverconfig.lua
@@ -27,7 +27,7 @@ local config =
 	
 	--Minimum amount of votes (in percent of all players) needed for a valid vote/poll. If the percentage of votes
 	--is lower than this value, the result will be discarded. Value must be between 0 and 100.
-	min_votes_in_percent		= 25,
+	min_votes_in_percent		= 50,
 	
 	--Time after starting a vote that a player cannot initiate another vote of the same kind
 	--(i.e. votekick or missionvote/-poll). In minutes. Must be >= 0. Default is 5 minutes.
@@ -49,7 +49,7 @@ local config =
 	miz_rotate_announcement = { 1,2,3,4,5,10,15,60 },
 	
 	--If true the current mission will be paused once all players left the server. Set to false to disable.
-	pause_if_server_empty		= true,
+	pause_if_server_empty		= false,
 	
 	--If true the current mission will be unpaused when a new player enters an empty server. A mission will also be
 	--resumed ~1min after loading if the server is not empty. Set to false to disable.
@@ -57,13 +57,13 @@ local config =
 
 	--If true the server automatically restarts the current mission when all players left the server.
 	--Set to false to disable.
-	restart_if_server_empty		= true,
+	restart_if_server_empty		= false,
 	
 	--Number of human teamkills after which a player will be kicked automatically. Set to 0 to disable.
 	kick_after_teamkills		= 1,
 	
 	--Number of AI teamkills incidents after which a player will be kicked automatically. Set to 0 to disable.
-	kick_after_AI_teamkills		= 1,
+	kick_after_AI_teamkills		= 2,
 	
 	--Number of friendly fire (damaging human teammates) incidents after which a player will be kicked automatically. Set to 0 to disable.
 	kick_after_friendly_fire 	= 1,
@@ -81,7 +81,7 @@ local config =
 	
 	--Score below which the player will be kicked automatically. Must be negative. Set to 0 to disable.
 	--Can be used together with "kick_after_teamkills" and/or "kick_after_AI_teamkills" and/or "kick_after_friendly_fire".
-	kick_below_score			= -10,
+	kick_below_score			= 0,
 	
 	--Number of high average ping warnings after which a player will be kicked.
 	--Set to 0 to disable ping logging and kicking for high ping.
@@ -89,14 +89,14 @@ local config =
 	
 	--Maximum average ping. Average is calculated over the last 100 ping values, and only for non-spectating players (excluding subadmins).
 	--Must be positive, default is 500. Option is only relevant when kick_after_max_ping_events > 0.
-	max_average_ping			= 300,
+	max_average_ping			= 500,
 	
 	--Penalty time in minutes that a player cannot reconnect after being kicked. Set to 0 to disable.
-	wait_after_kick				= 15,
+	wait_after_kick				= 60,
 	
 	--Number of kicks after which a player will be banned automatically.
 	--The player's IP will be added to local banlist. Set to 0 to disable.
-	autoban_after_kicks			= 3,
+	autoban_after_kicks			= 2,
 	
 	--URL of masterbanlist. Set to "" to disable. URL format: "[http://][<user>[:<password>]@]<host>[:<port>][/<path>]"
 	--Ex.1: "http://BartS:AyCaramba@www.abc-xyz.net/def/banlist.txt" (if authorization via .htaccess is required)
@@ -116,14 +116,14 @@ local config =
 	bankick_enabled					= true,
 	
 	--Message of the Day. Use * to indicate line breaks. Set to "" to disable.
-	MOTD						= "*MOTD: Type /help into chat to see enabled server commands*You can use these commands to start a missionvote or a votekick!",
+	MOTD						= "*Welcome to the 159th DCS World server.*Type /help into chat to see enabled server commands*You can restart or change Missions!*159th TSv3: 78.129.193.145:10119 pw=jacksparrow",
 	
 	--Time interval in minutes between displaying the message of the day. MOTD will also be shown 2 mins after a new player
 	--connected. If MOTD is disabled, only the time remaining until mission rotation/restart is displayed (if enabled)
-	MOTD_interval				= 20,
+	MOTD_interval				= 15,
 	
 	--Server rules as shown by /rules command. Use * to indicate line breaks.
-	server_rules				= "1. No teamkilling*2. Do not damage teammates*3. No bad language*4. Treat each other with respect",
+	server_rules				= "1. No teamkilling*2. Do not damage teammates*3. No bad language*4. Treat each other with respect*5.  Enjoy!",
 
 	--Interval of frames after which ServMan checks if scheduled events should be triggered
 	--Lower values may (or may not) decrease server performance, higher values may delay scheduled events.
@@ -165,7 +165,7 @@ local config =
 	--   - username = "Champions"
 	--   - Password = "IneverCrash!"
 
-	squad_login_enable			= false,
+	squad_login_enable			= true,
 	squad_prefix				= "",
 	squad_suffix				= "",
 	squad_username				= "",
@@ -182,8 +182,9 @@ local config =
 	-- If you set kickbanlevel = 2, then everyone including squad-members can 
 	-- be kicked/banned for offences. Admins cannot be kicked banned at 
 	-- level 2, in that case you should set kickbanlevel = 3
-	kickbanlevel		= 2
+	kickbanlevel		= 2,
 	
+	missionfolder		= lfs.writedir() .. '\Missions', -- not tested yet with DCS 1.5
 }
 
 
